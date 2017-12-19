b84ba0e4ad3779753c2bea6a645d4f5fa000c854
Tarciso <tarcisoloiola@hotmail.com>
2017-12-19 18:17:28 -0500

    commit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Article: require("./Article.js"),
-  Saved: require("./Saved.js"),
-  Note: require("./Note.js")
+  Article: require("./Article"),
+  Saved: require("./Saved"),
+  Note: require("./Note")
 };
